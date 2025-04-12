106fd17fa166179ec5c8d2df78e746bd0db131c4
kartik221a <kartikkumar221a@gmail.com>
2025-04-13 03:49:01 +0530

    remove experimentals from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactRefresh: true, // Ensure Fast Refresh is enabled
-  },
 };
 
 export default nextConfig;

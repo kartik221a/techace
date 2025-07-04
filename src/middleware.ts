ace0ee4c0f5715945d7c697254f1aa0d9a33626d
Kartik Kumar <kartikkumar221a@gmail.com>
2025-07-04 12:51:32 +0000

    debug matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,12 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|j
-    // Always run for API routess(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  // Skip Next.js internals and all static files, unless found in search params
+  '/((?!_next|[^?]*\\.(?:html?|css|json|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  
+  // Always run for API routes
+  '/(api|trpc)(.*)',
+],
+
 };
 

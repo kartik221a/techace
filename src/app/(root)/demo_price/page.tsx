92f8a33c63ada00e69a4f1072e76ac546f3fda9b
Kartik Kumar <kartikkumar221a@gmail.com>
2025-07-04 12:35:29 +0000

    demo subscripton

---
+++ src/app/(root)/demo_price/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
+import { PriceTable } from "@clerk/nextjs"; // Import PriceTable from Clerk
+
+export default function Home() {
+  return (
+    <PriceTable />
+  );
+}

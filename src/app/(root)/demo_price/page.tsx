434e3fc6f10efc49ab56e1225ec4d3b09ae7b110
Kartik Kumar <kartikkumar221a@gmail.com>
2025-07-04 12:48:02 +0000

    debug vercel error PricingTable

--- src/app/(root)/demo_price/page.tsx
+++ src/app/(root)/demo_price/page.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
-import { PriceTable } from "@clerk/nextjs"; // Import PriceTable from Clerk
+import { PricingTable  } from "@clerk/nextjs"; // Import PriceTable from Clerk
 
 export default function Home() {
   return (
-    <PriceTable />
+    <PricingTable  />
   );
 }
